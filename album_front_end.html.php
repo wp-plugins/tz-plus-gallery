2240deb16e7fc8efd52dbc6d9d1e98c522f35439
tuyennv <tuyennv@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-23 07:02:34 +0000

    Version 1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/tz-plus-gallery/tags/1.0.1@1204573 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ album_front_end.html.php
@@ -0,0 +1,108 @@
+<?php
+
+function front_end_base_album($album_ids,$album_name, $album_type, $album_id,$album_data_type,$album_single_id,$album_include,
+                              $album_exclude, $album_limit, $album_single_limit,$options_color,$options_columns,$options_padding)
+{
+    wp_enqueue_style("plusgallery_css", plugins_url("css/plusgallery.css", __FILE__), FALSE);
+    wp_enqueue_script( 'plusgallery', plugins_url("js/plusgallery.js", __FILE__), array(), '1.0.0', true );
+    $width = 100/$options_columns;
+ ob_start();?>
+
+    <style type="text/css">
+        body #plusgallery<?php echo esc_attr($album_ids);?> a:hover, body  #pgzoomview.plusgallery<?php echo esc_attr($album_ids);?> a:hover,
+        body #plusgallery<?php echo esc_attr($album_ids);?> .pgalbumlink:hover .pgplus,
+        #plusgallery<?php echo esc_attr($album_ids);?> #pgthumbcrumbs li#pgthumbhome:hover{
+            background-color:<?php echo esc_attr($options_color); ?>
+        }
+        body #plusgallery<?php echo esc_attr($album_ids);?> .pgthumb, body #plusgallery<?php echo esc_attr($album_ids);?> .pgalbumthumb{
+            width:<?php echo esc_attr($width);?>%;
+            margin:0;
+            padding:<?php echo esc_attr($options_padding);?>;
+            max-width:none;
+            height:auto;
+        }
+
+    </style>
+
+
+    <?php
+if($album_type=='googleplus'){ ?>
+    <div id="plusgallery<?php echo esc_attr($album_ids);?>" class="plusgallery"
+         data-userid="<?php echo esc_attr($album_id); ?>"
+        <?php if($album_data_type=='single_album'){ ?>
+            data-album-id="<?php echo esc_attr($album_single_id); ?>"
+            data-limit="<?php echo esc_attr($album_single_limit); ?>"
+        <?php } ?>
+        <?php if($album_data_type=='multi_album'){ ?>
+            data-include="<?php echo esc_attr($album_include); ?>"
+            data-exclude="<?php echo esc_attr($album_exclude); ?>"
+            data-album-limit="<?php echo esc_attr($album_limit); ?>"
+            data-album-title="true"
+        <?php } ?>
+         data-type="google">
+
+    </div>
+<?php } ?>
+
+    <?php
+if($album_type=='instagram'){?>
+    <div id="plusgallery<?php echo esc_attr($album_ids);?>" class="plusgallery"
+         data-userid="<?php echo esc_attr($album_id); ?>"
+         data-limit="<?php echo esc_attr($album_single_limit); ?>"
+         data-access-token="35163631.3a5ca5d.be9b66ad0f964d17b996d2a899bc8cd3"
+         data-type="instagram">
+
+    </div>
+<?php } ?>
+
+    <?php
+if($album_type=='flickr'){?>
+    <div id="plusgallery<?php echo esc_attr($album_ids);?>" class="plusgallery"
+         data-userid="<?php echo esc_attr($album_id); ?>"
+         <?php if($album_data_type=='single_album'){ ?>
+             data-album-id="<?php echo esc_attr($album_single_id); ?>"
+             data-limit="<?php echo esc_attr($album_single_limit); ?>"
+
+         <?php } ?>
+         <?php if($album_data_type=='multi_album'){ ?>
+             data-include="<?php echo esc_attr($album_include); ?>"
+             data-exclude="<?php echo esc_attr($album_exclude); ?>"
+             data-album-limit="<?php echo esc_attr($album_limit); ?>"
+             data-album-title="true"
+         <?php } ?>
+             data-api-key="c07f724ab7ed6a1b01b799fe753c6d13"
+
+             data-type="flickr">
+
+    </div>
+<?php } ?>
+
+    <?php
+if($album_type=='facebook'){ ?>
+    <div id="plusgallery<?php echo esc_attr($album_ids);?>" class="plusgallery"
+         data-userid="<?php echo esc_attr($album_id); ?>"
+         <?php if($album_data_type=='single_album'){ ?>
+             data-album-id="<?php echo esc_attr($album_single_id); ?>"
+             data-limit="<?php echo esc_attr($album_single_limit); ?>"
+         <?php } ?>
+         <?php if($album_data_type=='multi_album'){ ?>
+             data-include="<?php echo esc_attr($album_include); ?>"
+             data-exclude="<?php echo esc_attr($album_exclude); ?>"
+             data-album-limit="<?php echo esc_attr($album_limit); ?>"
+             data-album-title="true"
+         <?php } ?>
+            data-access-token="150849908413827|uYDHoXrvPZOLkQ-zRz_XoYdEeYM"
+             data-type="facebook">
+
+    </div>
+<?php } ?>
+    <script type="text/javascript">
+        jQuery(document).ready(function(){
+          jQuery('#plusgallery<?php echo esc_attr($album_ids);?>').plusGallery();
+        })
+    </script>
+ 	  <?php
+	return ob_get_clean();
+}  
+?>
+
---
+++ readme.txt
@@ -0,0 +1,100 @@
+=== TZ Plus Gallery ===
+Contributors: tuyennv , templaza
+Donate link: http://www.templaza.com/
+Tags: plugins, social plugin, gallery, social gallery, facebook album, google album, instagram album, flickr album.
+Requires at least: 3.0.1
+Tested up to: 4.2.2
+Stable tag: 4.3
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+TZ Plus Gallery - Display social gallery like Facebook, Flickr, Instagram and Google+.
+
+== Description ==
+
+TZ Plus Gallery display all your albums from social source Facebook, Flickr, Instagram, Google+ on your site.
+ You can post your photos every where, every time from your mobile device to Facebook, Flickr, Instagram and Google+. it will auto added on your website.
+<h4>Features</h4>
+<ul>
+<li>Display your albums from online source like Facebook, Instagram, Google + or Flickr to your site's gallery.</li>
+<li>Manage gallery by including/excluding albums, limit the number of pictures to be display in each album.</li>
+<li>Unlimited gallery display on your site.</li>
+<li>Display multiple albums in one page or post.</li>
+<li>Compatible with Tablet, Smart phone.</li>
+<li>Compatible with Visual Composer plugin.</li>
+<li>Options choose number of columns.</li>
+<li>Options choose color style.</li>
+<li>Options padding of albums or photos.</li>
+</ul>
+
+<h4>Demo</h4>
+
+[Facebook Fanpage Albums](http://wordpress.templaza.net/plugins/ "Facebook Fanpage Albums")
+<br>
+[Google+ Albums](http://wordpress.templaza.net/plugins/google-album/ "Google+ Albums")
+<br>
+[Instagram Albums](http://wordpress.templaza.net/plugins/instagram-album/ "Instagram Albums")
+<br>
+[Flickr Albums](http://wordpress.templaza.net/plugins/flickr-album/ "Flickr Albums")
+
+== Installation ==
+
+<h4>1. Automatic installation:</h4>
+
+The simplest way to install is to click on 'Plugins' then 'Add new' and type 'TZ Plus Gallery' in the search field.
+
+<h4>2. Manual installation 1:</h4>
+<ol>
+<li>Login to your website and go to the Plugins.</li>
+<li>Click the Add New button.</li>
+<li>Under Install Plugins, click the Upload Plugin.</li>
+<li>Select the plugin zip file (tz-plus-gallery.zip) from your computer then click the Install Now button.</li>
+<li>You should see a message stating that the plugin was installed successfully.</li>
+<li>Click the Activate Plugin.</li>
+</ol>
+
+<h4>2. Manual installation 2:</h4>
+<ol>
+<li>You should have access to the server where WordPress is installed.</li>
+<li>Upload the plugin zip file (tz-plus-gallery.zip) up to your server path(/wp-content/plugins/) and unzip it here.</li>
+<li>Login to your website and go to the Plugins section of your admin panel.</li>
+<li>Look for "TZ PlusGallery" and click Activate.</li>
+</ol>
+
+== Frequently Asked Questions ==
+
+= How to  add Facebook Fanpage Albums? =
+
+[youtube https://www.youtube.com/watch?v=kJPisKHOCEE]
+
+= How to add Flickr Albums? =
+
+[youtube https://www.youtube.com/watch?v=1yenWbddNmY]
+
+= How to add Instagram Albums? =
+
+[youtube https://www.youtube.com/watch?v=4KbqBXED_b8]
+
+= How to add Google+ Albums? =
+
+[youtube https://www.youtube.com/watch?v=YFHPPALjGcY]
+ 
+== Screenshots ==
+
+1. Create Albums
+2. Insert Album.
+3. Albums list.
+4. Insert Albums in post or page.
+5. Instagram albums display.
+6. Google+ Albums display.
+7. View photo in album.
+
+== Changelog ==
+
+= 1.0.0 =
+* Start Version
+
+== Upgrade Notice ==
+
+= 1.0.0 =
+* Start Version
